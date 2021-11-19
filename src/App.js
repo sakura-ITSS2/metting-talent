e89504c06f6c9da56b51b33d238760a9e25d50c8
huy-xu <nhathuyhedspi@gmail.com>
2021-11-19 16:45:28 +0700

    Create Login + Signup UI

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import Home from './components/Home';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import ManagerHome from './components/ManagerHome';
-import Talent from "./pages/Talent";
-import ManagerRoute from "./routes/ManagerRoute";
-import TalentRoute from "./routes/TalentRoute";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import Login from './components/Login/Login';
+import ManagerHome from './components/ManagerHome';
+import SignUp from './components/Signup/Signup';
+import Talent from './pages/Talent';
+import ManagerRoute from './routes/ManagerRoute';
+import TalentRoute from './routes/TalentRoute';
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Switch>
-          {/* public routes */}
-          <Route path="/" exact component = {Home}></Route>
-          <Route path="/login" component = {Login}></Route>
-          <Route path="/signup" component = {Signup}></Route>
+    return (
+        <div className="App">
+            <Router>
+                <Switch>
+                    {/* public routes */}
+                    <Route path="/" exact component={Home}></Route>
+                    <Route path="/login" component={Login}></Route>
+                    <Route path="/signup" component={SignUp}></Route>
 
-          {/* Manager routes */}
-          <ManagerRoute path="/manager" exact component = {ManagerHome}></ManagerRoute>
+                    {/* Manager routes */}
+                    <ManagerRoute
+                        path="/manager"
+                        exact
+                        component={ManagerHome}
+                    ></ManagerRoute>
 
-          {/* Talent routes */}
-          <TalentRoute path="/talent" component = {Talent}></TalentRoute>
-        </Switch>
-      </Router>
-    </div>
-  );
+                    {/* Talent routes */}
+                    <TalentRoute
+                        path="/talent"
+                        component={Talent}
+                    ></TalentRoute>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
