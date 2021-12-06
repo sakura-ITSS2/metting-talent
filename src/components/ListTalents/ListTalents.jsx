cc6b3b70ff035e5032c5ab48d51c7cc0dc5762cc
huy-xu <nhathuyhedspi@gmail.com>
2021-12-06 09:46:59 +0700

    Fix show post title

--- src/components/ListTalents/ListTalents.jsx
+++ src/components/ListTalents/ListTalents.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Image } from 'react-bootstrap';
 import { Link, useParams } from 'react-router-dom';
 import defaultAvatar from '../../images/default-avatar.png';
-import { getListTalents } from '../../Services/PostService';
+import { getListTalents, getPostTitle } from '../../Services/PostService';
 import {
     STATUS_ACCEPT,
     STATUS_DECLINE,
@@ -10,18 +10,20 @@ import {
 } from '../../utils/constants';
 import Header from '../Header/Header';
 import './ListTalents.scss';
-import {DetailTalent} from "../DetailTalent/DetailTalent";
 
 function ListTalents(props) {
     const [listTalents, setListTalents] = useState([]);
-    const {id} = useParams();
+    const [postTitle, setPostTitle] = useState('');
+    const { id } = useParams();
 
     useEffect(() => {
         const fetchlistTalents = async (id) => {
             const data = await getListTalents(id);
-            if(data){
-                setListTalents(data)
+            const title = await getPostTitle(id);
+            if (data) {
+                setListTalents(data);
             }
+            setPostTitle(title);
         };
 
         fetchlistTalents(id);
@@ -31,14 +33,14 @@ function ListTalents(props) {
         <div className="wrapper">
             <Header />
             <div className="list-talents-container">
-                <h2>{id}</h2>
+                <h2>{postTitle}</h2>
                 <div className="list-talents">
                     {listTalents?.map((talent) => (
                         <div className="talent-item" key={talent.id_talent}>
                             <Link
                                 to={{
                                     pathname: `detail-talent/${talent.id_talent}`,
-                                    idPost: id
+                                    idPost: id,
                                 }}
                                 className="profile"
                                 params={{ idPost: id }}
@@ -67,7 +69,7 @@ function ListTalents(props) {
                             <Link
                                 to={{
                                     pathname: `detail-talent/${talent.id_talent}`,
-                                    idPost: id
+                                    idPost: id,
                                 }}
                                 className={`status--${talent.status}`}
                                 params={{ idPost: id }}
